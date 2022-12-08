2869001d03c8591bdbe1d3788b41cb458ade32f3
hazym1nd <rei.navasero@gmail.com>
2022-12-08 16:50:35 +0800

    master neil Halp me

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import React from 'react';
 import Screen from '../components/Screen';
 import Button from '../components/Button';
@@ -16,20 +16,44 @@ const SettingsScreen = ({navigation}) => {
   return (
     <>
       <Screen>
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
-      <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
+        
+        <View style={styles.flex}>
+          <View >
+          <Image source={IMAGES.ic_star} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_share_desktop} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_phone} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_info} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_bookmark} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_document} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_check_list} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_question} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_robot} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_paint} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_unlock} style={styles.iconers} width={30} height={40} />
+          <Image source={IMAGES.ic_browser} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_user} style={styles.iconers} width={30} height={30} />
+          <Image source={IMAGES.ic_profile} style={styles.iconers} width={30} height={30} />
+          </View>
+        <View style={styles.margin}>
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={20} height={20} />
+          <Image source={IMAGES.ic_proceed} style={styles.iconers} width={18} height={20} />
+        </View>
+        </View>
+
+        <View style={styles.div}/>
+      
+
         {/* <Button
           text={'BACK'}
           onPress={handleBack}
@@ -44,4 +68,25 @@ const SettingsScreen = ({navigation}) => {
 
 export default SettingsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flex:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  margin:{
+    top: 10
+    
+  },
+  iconers:{
+    marginTop: 10,
+  },
+  div :{
+    marginVertical: 20,
+    padding: 15,
+    marginHorizontal: 20,
+    borderColor: 'white',
+    backgroundColor: 'rgba(217,217,217,0.5)',
+    borderRadius: 25,
+  },
+});
